fc129ccc83992c5628e72d494f586c5ec8e9e998
mskogfeldt <50482435+mskogfeldt@users.noreply.github.com>
2020-10-01 17:44:12 +0200

    some changes

---
+++ week3/w3d1e4/w3d1e4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace w3d1e4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var streams = new List<int> { };
+            var symbols = "!@#$%^&*()_+-=[];',.\\/~{}:|<>?";
+            var random = new Random();
+
+            for (int i = 0; i < 10; i++) streams.Add(random.Next(0, 80));
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+
+            while (true)
+            {
+                for (int x = 0; x < 80; x++)
+                {
+                    Console.Write(streams.Contains(x) ? symbols[random.Next(symbols.Length)] : ' ');
+                }
+
+                Console.WriteLine();
+                Thread.Sleep(100);
+                if (random.Next(0, 3) == 0) streams.RemoveAt(random.Next(streams.Count));
+                if (random.Next(0, 3) == 0) streams.Add(random.Next(0, 80));
+
+            }
+
+        }
+
+    }
+}
