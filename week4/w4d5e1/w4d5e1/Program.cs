859cafbeb5fa06e3bdb18c0ce46b879ea5e6bc63
mskogfeldt <50482435+mskogfeldt@users.noreply.github.com>
2020-10-06 14:41:40 +0200

    1.0

---
+++ week4/w4d5e1/w4d5e1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+
+namespace Chart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = new List<double>();
+            var random = new Random();
+
+            for (int i = 0; i < 70; i++)
+            {
+                data.Add(random.Next(20));
+                DisplayData(data);                
+            }
+        }
+
+        static void DisplayData(List<double> data)
+        {
+            Console.CursorVisible = false;
+            Console.SetCursorPosition (0, 0);
+            //public static void SetCursorPosition (int left, int top);
+
+            for (int y = 20; y >= 0; y--)
+            {
+                if (y % 5 == 0)
+                {
+                    Console.Write($"{y,3} |");
+                }
+                else
+                {
+                    Console.Write("    |");
+                }
+
+                for (int x = 0; x < data.Count; x++)
+                {
+                    if (y == 0)
+                    {
+                        Console.Write("-");
+                        continue;
+                    }
+
+                    Console.Write(y <= data[x] ? "\u2592" : " ");
+                }
+
+                Console.WriteLine();
+            }
+
+            Thread.Sleep(10);
+        }
+    }
+}
