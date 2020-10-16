576c2a371653308b18b2fa17a2b88b39c6dfb087
mskogfeldt <50482435+mskogfeldt@users.noreply.github.com>
2020-10-16 03:38:07 +0200

    2.0

--- week3/w3d5m1/w3d5m1/Service.cs
+++ week3/w3d5m1/w3d5m1/Service.cs
@@ -7,11 +7,18 @@ namespace w3d5m1
     class Service
     {
         public int score = 0;
+        Random random = new Random();
 
         public void addToScore(int pointsEarned)
         {
             score += pointsEarned;
         }
+
+/*
+        static int KnockPinOnPath(int path, List<int> pinsStanding)
+        {
+            return 0;
+        }*/
 
 
         public int calculateScoreFirstThrow(List<int> standingPinns)
@@ -34,21 +41,87 @@ namespace w3d5m1
 
             return results - alreadyKnockedDown;
         }
-
-        public void knockPins(List<int> pins, int lane)
+/*
+        public int throwBall(int lane)
         {
-            if (lane < 1 || lane > 7) Console.WriteLine("Ha MISS!");
-            if (lane == 1) pins[6] = 0;
-            if (lane == 2) pins[3] = 0;
-            if (lane == 3 && pins[1] == 0) pins[7] = 0;
-            if (lane == 3 && pins[1] > 0) pins[1] = 0;
-            if (lane == 4 && pins[0] == 0) pins[4] = 0;
-            if (lane == 4 && pins[0] > 0) pins[0] = 0;
-            if (lane == 5 && pins[2] == 0) pins[8] = 0;
-            if (lane == 5 && pins[2] > 0) pins[2] = 0;
-            if (lane == 6) pins[5] = 0;
-            if (lane == 7) pins[9] = 0;
 
+        }*/
+
+        public int doesObjectChangeLaneAfterImpact(int lane)
+        {
+            int procentige = random.Next(1, 101);
+            if (procentige < 34) lane --;
+            if (procentige < 68) lane += 0;
+            if (procentige > 67) lane ++;
+            return lane;
+
+        }
+
+        public void knockPins(List<int> pins, int lane, int coordinateY)
+        {
+            int rangeTravled = coordinateY;
+            if (lane == 4 && pins[0] == 0)
+            {
+                pins[0] = 0;
+                int newLane1 = doesObjectChangeLaneAfterImpact(lane);
+                int newLane2 = doesObjectChangeLaneAfterImpact(lane);
+                rangeTravled++;
+                knockPins(pins, newLane1, rangeTravled);
+                knockPins(pins, newLane2, rangeTravled);
+            }
+            else rangeTravled++;
+            if (lane == 3 && pins[1] > 0 && rangeTravled == 1)
+            {
+                pins[1] = 0;
+                int newLane1 = doesObjectChangeLaneAfterImpact(lane);
+                int newLane2 = doesObjectChangeLaneAfterImpact(lane);
+                rangeTravled++;
+                knockPins(pins, newLane1, rangeTravled);
+                knockPins(pins, newLane2, rangeTravled);
+            }
+            else if (lane == 5 && pins[2] > 0 && rangeTravled == 1)
+            {
+                pins[2] = 0;
+                int newLane1 = doesObjectChangeLaneAfterImpact(lane);
+                int newLane2 = doesObjectChangeLaneAfterImpact(lane);
+                rangeTravled++;
+                knockPins(pins, newLane1, rangeTravled);
+                knockPins(pins, newLane2, rangeTravled);
+            }
+            else rangeTravled++;
+            if (lane == 2 && pins[3] > 0 && rangeTravled < 3)
+            {
+                rangeTravled = 2;
+                pins[3] = 0;
+                int newLane1 = doesObjectChangeLaneAfterImpact(lane);
+                int newLane2 = doesObjectChangeLaneAfterImpact(lane);
+                rangeTravled++;
+                knockPins(pins, newLane1, rangeTravled);
+                knockPins(pins, newLane2, rangeTravled);
+            }
+            else if (lane == 4 && pins[4] > 0 && rangeTravled == 2)
+            {
+                pins[4] = 0;
+                int newLane1 = doesObjectChangeLaneAfterImpact(lane);
+                int newLane2 = doesObjectChangeLaneAfterImpact(lane);
+                rangeTravled++;
+                knockPins(pins, newLane1, rangeTravled);
+                knockPins(pins, newLane2, rangeTravled);
+            }
+            else if (lane == 6 && pins[5] > 0 && rangeTravled == 2)
+            {
+                pins[5] = 0;
+                int newLane1 = doesObjectChangeLaneAfterImpact(lane);
+                int newLane2 = doesObjectChangeLaneAfterImpact(lane);
+                rangeTravled++;
+                knockPins(pins, newLane1, rangeTravled);
+                knockPins(pins, newLane2, rangeTravled);
+            }
+            else rangeTravled ++;
+            if (lane == 1 && pins[6] > 0 && rangeTravled == 3) pins[6] = 0;
+            if (lane == 3 && pins[7] > 0 && rangeTravled == 3) pins[7] = 0;
+            if (lane == 5 && pins[8] > 0 && rangeTravled == 3) pins[8] = 0;
+            if (lane == 7 && pins[9] > 0 && rangeTravled == 3) pins[9] = 0;
         }
 
         public string takeIntMakeString(int number)
