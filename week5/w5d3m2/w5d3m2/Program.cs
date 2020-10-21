f8633a4fa214ab8c220e094ec6e9e6069b228724
mskogfeldt <50482435+mskogfeldt@users.noreply.github.com>
2020-10-21 02:08:14 +0200

    1.0

---
+++ week5/w5d3m2/w5d3m2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace w5d3m2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int i = 0;
+
+            while (i <= 22)
+            {
+                Console.Write(i + " = " + (char)i);
+
+                if (i < 22)
+                {
+                    Console.Write((char)10);
+                }
+
+                i++;
+            }
+
+            Console.Write("Please press any key to turn page");
+
+            Console.ReadKey();
+
+            while (i > 22 && i <= 44)
+            {
+                Console.Write(i + " = " + (char)i);
+
+                if (i < 44)
+                {
+                    Console.Write((char)10);
+                }
+
+                i++;
+            }
+
+            Console.Write("Please press any key to turn page");
+
+            Console.ReadKey();
+        }
+    }
+}
