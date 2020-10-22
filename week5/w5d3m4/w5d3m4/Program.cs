a0562f8e0e499f596f91c23aa656ca2ccd1773ca
mskogfeldt <50482435+mskogfeldt@users.noreply.github.com>
2020-10-22 17:02:22 +0200

    1,05

--- week5/w5d3m4/w5d3m4/Program.cs
+++ week5/w5d3m4/w5d3m4/Program.cs
@@ -7,48 +7,65 @@ namespace w5d3m4
     {
         static void Main(string[] args)
         {
-            var random = new Random();
 
-            Console.WriteLine("Throwing 1d6 " + DiceRoll("Throwing 1d6"));
-            Console.WriteLine("Throwing 2d8 " + DiceRoll("Throwing 2d8"));
-            Console.WriteLine("Throwing 3d6+8 " + DiceRoll("Throwing 3d6+8"));
-            Console.WriteLine("Throwing 1d4+4 " + DiceRoll("Throwing 1d4+4"));
-           
-    
 
-            int DiceRoll(string diceNotation)
+            while (true)
             {
-                if (findNumberOfIntsInString(diceNotation) < 3) return findInt(diceNotation, 1) * random.Next(1, findInt(diceNotation, 2));
-                if (findNumberOfIntsInString(diceNotation) > 2) return findInt(diceNotation, 1) * random.Next(1, findInt(diceNotation, 2)) + findInt(diceNotation, 3);
-                return -1;
-            }
-    
-            int findNumberOfIntsInString(string diceNotation)
-            {
-                int count = 0;
-                foreach (char c in diceNotation)
-                {
-                    if (c >= '1' && c <= '9') count ++;
-                }
-                return count;
+                Console.WriteLine("Throwing 1d6 " + DiceRoll("1d6"));
+                Console.WriteLine("Throwing 2d8 " + DiceRoll("2d8"));
+                Console.WriteLine("Throwing 3d6+8 " + DiceRoll("3d6+8"));
+                Console.WriteLine("Throwing 1d4+4 " + DiceRoll("1d4+4"));
+                Console.ReadLine();
+                Console.Clear();
             }
 
-            int findInt(string diceNotation, int index)
+
+        }
+
+        static Random random = new Random();
+        static int DiceRoll(int numberOfDices, int sidesOfDice, int fixedBonus = 0)
+        {
+            int dicescore = 0;
+            for (int i = 0; i < numberOfDices; i++)
             {
-                int count = 0;
-                foreach(char c in diceNotation)
+                dicescore += random.Next(1, sidesOfDice + 1);
+            }
+            dicescore += fixedBonus;
+            return dicescore;
+        }
+
+        static int DiceRoll(string diceNotation)
+        {
+            if (findNumberOfIntsInString(diceNotation) < 3) return DiceRoll(findInt(diceNotation, 1), findInt(diceNotation, 2));
+            if (findNumberOfIntsInString(diceNotation) > 2) return DiceRoll(findInt (diceNotation, 1), findInt(diceNotation, 2), findInt(diceNotation, 3));
+            return -1;
+        }
+
+        static int findNumberOfIntsInString(string diceNotation)
+        {
+            int count = 0;
+            foreach (char c in diceNotation)
+            {
+                if (c >= '1' && c <= '9') count++;
+            }
+            return count;
+        }
+
+        static int findInt(string diceNotation, int index)
+        {
+            int count = 0;
+            foreach (char c in diceNotation)
+            {
+                if (c >= '1' && c <= '9')
                 {
-                    if (c >= '1' && c <= '9') 
+                    count++;
+                    if (count == index)
                     {
-                        count++; 
-                        if (count == index)
-                        {
-                            return Convert.ToInt32(new string(c, 1));
-                        }    
-                    } 
+                        return Convert.ToInt32(new string(c, 1));
+                    }
                 }
-                return -1;
             }
+            return -1;
         }
     }
 }
