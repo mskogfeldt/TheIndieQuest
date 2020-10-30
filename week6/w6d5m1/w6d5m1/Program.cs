ba6d807ecc7d63b098b66e9631f1f1e10cf2dc0f
mskogfeldt <50482435+mskogfeldt@users.noreply.github.com>
2020-10-30 04:17:21 +0100

    0,25

---
+++ week6/w6d5m1/w6d5m1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace w6d5m1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] levelLines = File.ReadAllLines("MazeLevel.txt");
+            
+            
+            /*
+            foreach (string s in levelLines)
+            {
+                Console.Write(s);
+            }
+            */
+        }
+    }
+}
