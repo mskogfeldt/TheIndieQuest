a5e535cffbdf2a4791f3ad33ab54c50eed915cfb
mskogfeldt <50482435+mskogfeldt@users.noreply.github.com>
2020-11-02 00:00:04 +0100

    1..0

--- week6/w6d5m1/w6d5m1/Program.cs
+++ week6/w6d5m1/w6d5m1/Program.cs
@@ -8,14 +8,46 @@ namespace w6d5m1
         static void Main(string[] args)
         {
             string[] levelLines = File.ReadAllLines("MazeLevel.txt");
-            
-            
-            /*
-            foreach (string s in levelLines)
+
+            int playerPossitionY = 0;
+            int playerPossitionX = 0;
+
+            // foreach (string s in levelLines)
+            // {
+            Console.WriteLine(levelLines[0]);
+            Console.WriteLine(levelLines[1]);
+            Console.WriteLine(levelLines[2]);
+            Console.WriteLine(levelLines[3]);
+            Console.WriteLine(levelLines[4]);
+            Console.WriteLine(levelLines[5]);
+            Console.WriteLine(levelLines[6]);
+            string[] sortedNumbers = levelLines[1].Split('x');
+            //   }
+            int width = Int32.Parse(sortedNumbers[0]);
+            int height = Int32.Parse(sortedNumbers[1]);
+
+            char[,] mapOfMaze = new char[width, height];
+            /*foreach (string s in levelLines)
+             {
+                 Console.WriteLine(s);
+
+             }*/
+            int yCoordinateForLevelLines = 0;
+            for (int y = 2; y < height; y++)
             {
-                Console.Write(s);
+                for (int x = 0; x < width; x++)
+                {
+                    string temp = levelLines[y];
+                    mapOfMaze[x, y -2] = temp[x];
+                    if (temp[x] == 'S')
+                    {
+                        playerPossitionY = y - 2;
+                        playerPossitionX = x;
+                    }
+                }
             }
-            */
+            Console.WriteLine("playerPossitionY " + playerPossitionY);
+            Console.WriteLine("playerPossitionx " + playerPossitionX);
         }
     }
 }
