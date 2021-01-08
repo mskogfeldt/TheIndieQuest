87cae7d8886a74d35a37426a5c9b26c7f989826e
mskogfeldt <50482435+mskogfeldt@users.noreply.github.com>
2021-01-08 17:57:58 +0100

    1.0

---
+++ week6/w6d1m1/w6d1m1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace w6d1m1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string link = @"MonsterManual.txt";
+
+            string[] monsterNames = File.ReadAllLines(link);
+            
+            List<string> listOfMonsterNames = new List<string> { monsterNames[0] };
+
+            for (int i = 1; i < monsterNames.Length; i++)
+            {
+                if (monsterNames[i - 1] == "")
+                {
+                    listOfMonsterNames.Add(monsterNames[i]);
+                }
+            }
+
+            Console.WriteLine("Monsters in the manual are");
+            foreach (string name in listOfMonsterNames) Console.WriteLine(name);
+        }
+    }
+}
