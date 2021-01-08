271efdc028c334fe9de783a20c7dc19c27aa5411
mskogfeldt <50482435+mskogfeldt@users.noreply.github.com>
2021-01-08 22:41:27 +0100

    now its actualy mission 2

--- week6/w6d1m1/w6d1m1/Program.cs
+++ week6/w6d1m1/w6d1m1/Program.cs
@@ -10,20 +10,93 @@ namespace w6d1m1
         {
             string link = @"MonsterManual.txt";
 
-            string[] monsterNames = File.ReadAllLines(link);
-            
-            List<string> listOfMonsterNames = new List<string> { monsterNames[0] };
+            string[] monsterData = File.ReadAllLines(link);
 
-            for (int i = 1; i < monsterNames.Length; i++)
+            List<string> listOfMonsterNames = new List<string> { };
+
+            List<bool> listOfMonsterFlyingAbilety = new List<bool> { };
+
+            CreateListOfMonsterNames(listOfMonsterNames, monsterData);
+
+            CreateListOfMonsterFlyingAbilety(listOfMonsterFlyingAbilety, monsterData);
+
+            PringMonsterNamesAndFlytingAbilety(listOfMonsterNames, listOfMonsterFlyingAbilety);
+
+            void PringMonsterNamesAndFlytingAbilety(List<string> monsterNames, List<bool> monsterFlyingAbilety)
             {
-                if (monsterNames[i - 1] == "")
+                Console.WriteLine("Monsters in the manual are");
+                for (int i = 0; i < monsterNames.Count; i++)
                 {
-                    listOfMonsterNames.Add(monsterNames[i]);
+                    Console.WriteLine($"{listOfMonsterNames[i]} - Can fly: {monsterFlyingAbilety[i]}");
+                }
+
+            }
+
+            /* List<string> CreateListOfMonsterNames(string[] listOfMonsterData)
+             {
+                 List<string> monsterNames = new List<string> { listOfMonsterData[0] };
+                 for (int i = 1; i < listOfMonsterData.Length; i++)
+                 {
+                     if (listOfMonsterData[i - 1] == "")
+                     {
+                         listOfMonsterNames.Add(listOfMonsterData[i]);
+                     }
+                 }
+                 return monsterNames;
+             }
+            
+                List<bool> CreateListOfMonsterFlyingAbilety(string[] listOfMonsterData)
+            {
+                List<bool> monsterFlyingAbilety = new List<bool> { };
+                {
+                    for (int i = 1; i < listOfMonsterData.Length; i++)
+                    {
+                        if (listOfMonsterData[i - 1].Contains("Speed"))
+                        {
+                            if (listOfMonsterData[i - 1].Contains("fly"))
+                            {
+                                monsterFlyingAbilety.Add(true);
+                            }
+                            else
+                            {
+                                monsterFlyingAbilety.Add(false);
+                            }
+                        }
+                    }
+                }
+                return monsterFlyingAbilety;
+            }
+             
+             */
+            void CreateListOfMonsterNames(List<string> monsterNames, string[] listOfMonsterData)
+            {
+                for (int i = 1; i < listOfMonsterData.Length; i++)
+                {
+                    if (listOfMonsterData[i - 1] == "")
+                    {
+                        listOfMonsterNames.Add(listOfMonsterData[i]);
+                    }
                 }
             }
 
-            Console.WriteLine("Monsters in the manual are");
-            foreach (string name in listOfMonsterNames) Console.WriteLine(name);
+            void CreateListOfMonsterFlyingAbilety(List<bool> monsterFlyingAbilety, string[] listOfMonsterData)
+            {
+                for (int i = 1; i < listOfMonsterData.Length; i++)
+                {
+                    if (listOfMonsterData[i - 1].Contains("Speed"))
+                    {
+                        if (listOfMonsterData[i - 1].Contains("fly"))
+                        {
+                            monsterFlyingAbilety.Add(true);
+                        }
+                        else
+                        {
+                            monsterFlyingAbilety.Add(false);
+                        }
+                    }
+                }
+            }
+
         }
     }
 }
