df9409272b7c04f53307cd8e2134f21742060f78
mskogfeldt <50482435+mskogfeldt@users.noreply.github.com>
2021-01-09 23:22:30 +0100

    1.0

---
+++ week6/w6d1m4/w6d1m4/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace w6d1m4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string link = @"MonsterManual.txt";
+
+            string[] monsterData = File.ReadAllLines(link);
+
+            List<string> listOfMonsterNames = new List<string> { monsterData[0] };
+
+            List<bool> listOfMonsterWhoRollsTenOrMoreDiceForHp = new List<bool> { };
+
+            CreateListOfMonsterNames(listOfMonsterNames, monsterData);
+
+            // CreateListOfMonsterWhoRollsTenOrMoreDiceForHp(listOfMonsterWhoRollsTenOrMoreDiceForHp, monsterData);
+
+            PringMonsterNames(listOfMonsterNames);
+
+            void PringMonsterNames(List<string> monsterNames)
+            {
+                Console.WriteLine("Monsters that can fly 10-40 feet per turn:");
+                for (int i = 0; i < monsterNames.Count; i++)
+                {
+                    Console.WriteLine($"{monsterNames[i]}");
+                }
+
+
+            }
+
+            void CreateListOfMonsterNames(List<string> monsterNames, string[] listOfMonsterData)
+            {
+                string flySpeedFom10To39 = "fly [1-3][0-9][^\\d]";
+                string flySpeed40 = "fly 40[^\\d]";
+
+                for (int i = 1; i < listOfMonsterData.Length; i++)
+                {
+                    if (listOfMonsterData[i].Contains("Speed"))
+                    {
+                        if (Regex.IsMatch(listOfMonsterData[i], flySpeedFom10To39) || Regex.IsMatch(listOfMonsterData[i], flySpeed40))
+                        {
+                            listOfMonsterNames.Add(listOfMonsterData[i - 4]);
+                        }
+                    }
+                }
+            }
+
+            void CreateListOfMonsterWhoRollsTenOrMoreDiceForHp(List<bool> monsterHp, string[] listOfMonsterData)
+            {
+                string tenOrMoreRolls = "\\d\\dd";
+
+                for (int i = 1; i < listOfMonsterData.Length; i++)
+                {
+                    if (monsterData[i - 1].Contains("Hit Points"))
+                    {
+                        if (Regex.IsMatch(monsterData[i - 1], tenOrMoreRolls))
+                        {
+                            monsterHp.Add(true);
+                        }
+                        else
+                        {
+                            monsterHp.Add(false);
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+}
