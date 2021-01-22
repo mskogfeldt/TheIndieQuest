3f5e435d3a228d362435709bdf1db9988b8e9893
mskogfeldt <50482435+mskogfeldt@users.noreply.github.com>
2021-01-22 13:08:49 +0100

    with coments

--- week7/w7d2m1/w7d2m1/Program.cs
+++ week7/w7d2m1/w7d2m1/Program.cs
@@ -16,32 +16,45 @@ namespace w7d2m1
         public static void RunTheProgramLoop(string diceNotationGoingInToTheMethod)
         {
 
-            bool keepAtIt = true;
+            //default setting for variable used when pressenting options to know what to write to the use
             bool sucsessfullDiceNotation = true;
+         
+            // default setting in form a variable, to make sure you can either repeat with same dicenotation or do a new one
             string usersDiceNotation = "";
-            while (keepAtIt == true)
+           
+            while ( true)
             {
+                // Checking if there is no dicenotaion going in to the method, ie it is not repeated
                 if (diceNotationGoingInToTheMethod == "")
                 {
                     PrintOptionsForUser(sucsessfullDiceNotation);
                     usersDiceNotation = Console.ReadLine();
                 }
+                // then there is a repetion of earlier input, dont ask for new input from the user
                 else
                 {
                     usersDiceNotation = diceNotationGoingInToTheMethod;
                 }
+                //checking so that the user input is IsStandardDiceNotation dice notation.
                 if (IsStandardDiceNotation(usersDiceNotation))
                 {
                     Console.WriteLine("Simulating …");
+                    
+                    //reciving the sting dicenotation from user input in a list of two or three ints
                     List<int> dataDiceRoll = DiceRoll(usersDiceNotation);
 
+                    // taking the list of two or three ints and turning them in to a list of dicerolls with the sum at the last spot in the list
                     List<int> resultsOfDiceRoll = ExecutingDiceRoll(dataDiceRoll[0], dataDiceRoll[1], dataDiceRoll[2]);
 
+                    // printning results starts here, specoial graphics for d4(one row below), d6(two rows below) or any other results(three rows below)
                     if (dataDiceRoll[1] == 4) PrintTheResultsForD4(resultsOfDiceRoll);
                     else if (dataDiceRoll[1] == 6) PrintTheResultsForD6(resultsOfDiceRoll);
                     else PrintTheResults(resultsOfDiceRoll);
 
+                   //no change from default setting
                     sucsessfullDiceNotation = true;
+
+                    //checking what the user whants to do after writing results from diceroll
                     string whatToDo = QuitRepeatContinue();
                     if (whatToDo == "n") RunTheProgramLoop("");
                     else if (whatToDo == "q") break;
@@ -49,6 +62,7 @@ namespace w7d2m1
                 }
                 else
                 {
+                    //unsucessfull try, change bool to effect option for user 
                     sucsessfullDiceNotation = false;
                 }
             }
@@ -61,6 +75,7 @@ namespace w7d2m1
             return whatToDo;
         }
 
+        // for all results exept d4 and d6
         public static void PrintTheResults(List<int> infoOfDiceRoll)
         {
             int numberOfItemsInList = infoOfDiceRoll.Count;
@@ -72,6 +87,7 @@ namespace w7d2m1
             Console.WriteLine("You rolled " + infoOfDiceRoll[infoOfDiceRoll.Count - 1] + ".");
         }
 
+        //results for d4
         public static void PrintTheResultsForD4(List<int> infoOfDiceRoll)
         {
             string[] dices = new string[4];
@@ -93,7 +109,7 @@ namespace w7d2m1
 
         }
 
-
+        //results for d4
         public static void PrintTheResultsForD6(List<int> infoOfDiceRoll)
         {
             string[] dices = new string[7];
@@ -161,7 +177,7 @@ namespace w7d2m1
             Console.WriteLine("You rolled " + infoOfDiceRoll[infoOfDiceRoll.Count - 1] + ".");
         }
     
-
+        //Used to turn int to string and ad letter depending on number, then used in printing the results, like 1st, 2nd...
         public static string MakePresentableNumberOfInt(int number)
         {
             {
@@ -201,6 +217,7 @@ namespace w7d2m1
             Console.WriteLine("DICE SIMULATOR\n");
         }
 
+        //preseenting 2 options depending on a bool
         public static void PrintOptionsForUser(bool isItFirstTry)
         {
             if (isItFirstTry) Console.WriteLine("Enter desired dice roll in standard dice notation:");
@@ -222,6 +239,7 @@ namespace w7d2m1
             return false;
         }
 
+        //turning string(of user input) into 2 or 3 length list
         static List<int> DiceRoll(string diceNotation)
         {
             string[] values = diceNotation.Split('d', '+', '-');
@@ -249,6 +267,7 @@ namespace w7d2m1
             return results;
         }
 
+        //turning list of tho or three ints into list with reults of dicerolls, sum at the last place in list 
         public static List<int> ExecutingDiceRoll(int numberOfRolls, int diceSides, int fixedBonus = 0)
         {
             List<int> rollsAndSum = new List<int> { };
